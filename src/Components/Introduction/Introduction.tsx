8786b18c26f8d2ce68bea7b8d1b4aa68a2fe433e
Saurish Phatak <saurishphatak@gmail.com>
2022-07-19 12:19:07 +0530

    Added intro page. Software Developer, Concept Visualizer animation left

--- src/Components/Introduction/Introduction.tsx
+++ src/Components/Introduction/Introduction.tsx
@@ -1,7 +1,33 @@
 import "./Introduction.css";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { Typewriter } from "react-simple-typewriter";
 
 export default function Introduction() {
+
     return (
-        <div className="introduction" id="introduction">Introduction</div>
+        <div className="introduction" id="introduction">
+            <div className="left">
+                <div className="imgContainer"></div>
+            </div>
+
+            <div className="right">
+
+                <div className="introRightWrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Saurish Phatak</h1>
+                    <h3>
+                        {/*
+                        //! TODO : Add software dev/ concept visualizer
+                        //! animation
+                     */}
+
+                    </h3>
+                </div>
+
+                <a href="#projects">
+                    <KeyboardArrowDownIcon className='downIcon' />
+                </a>
+            </div>
+        </div >
     )
 }
