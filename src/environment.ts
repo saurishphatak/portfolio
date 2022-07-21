095dcdf0036f34e035d321a55f4193a80b8728db
Saurish Phatak <saurishphatak@gmail.com>
2022-07-21 18:29:20 +0530

    Added gitlab and linkedin links and svg icons

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     myEmail: 'saurishphatak@gmail.com',
+    gitlabLink: 'https://gitlab.com/saurish_phatak',
+    linkedinLink: 'https://www.linkedin.com/in/saurish-phatak/'
 
 }
