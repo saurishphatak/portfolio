79130bac7be544b5b88b0407c40ded7fbc42561f
Saurish Phatak <saurishphatak@gmail.com>
2022-09-17 10:51:19 +0530

    Tweaked resume

--- src/environment.ts
+++ src/environment.ts
@@ -3,4 +3,5 @@ export const environment = {
     gitlabLink: 'https://gitlab.com/saurish_phatak',
     linkedinLink: 'https://www.linkedin.com/in/saurish-phatak/',
     resumeLink: 'https://drive.google.com/file/d/1o5zhveqxaR6452gLpwubVHfb_CoCx5JP/view?usp=sharing'
+
 }
