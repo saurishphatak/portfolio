b6cc576ab8d69ad532aad77979dc711d77fc8869
Saurish Phatak <saurishphatak@gmail.com>
2024-01-12 20:09:22 +0530

    Changed resume link

--- src/environment.ts
+++ src/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     myEmail: 'saurishphatak@gmail.com',
     gitlabLink: 'https://gitlab.com/saurish_phatak',
     linkedinLink: 'https://www.linkedin.com/in/saurish-phatak/',
-    resumeLink: 'https://drive.google.com/file/d/12rgZIEKCBUyGpD-BAsJUlHgGILvGEo9q/view?usp=sharing'
+    resumeLink: 'https://drive.google.com/file/d/1yiy-qZmYihl8PhB3po7myHk3kKNx4W2R/view?usp=sharing'
 }
